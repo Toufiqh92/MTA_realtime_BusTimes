3ab9fc243c8e349d2fdfad0308abca164539c3f0
Devin Chen <Devinc23456@gmail.com>
2025-08-21 16:58:01 -0400

    Login API that connects to Supabase

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
-import supabase from "./supabaseClient"
+import supabase from "./supabaseClient.js"
 dotenv.config();
 
 const app = express();
